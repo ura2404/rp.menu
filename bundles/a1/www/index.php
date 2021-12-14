1b0acae421911eb8c54721a4dc73f69bc40eeedb
ura <ura@itx.ru>
2021-12-14 15:34:28 +0300

    .

--- bundles/a1/www/index.php
+++ bundles/a1/www/index.php
@@ -1,3 +1,27 @@
+<html>
+    <head>  
+        <link rel="stylesheet" type="text/css" href="css/main.css"/>        
+    </head>
+    <body>
+        <div id="video-bg">
+            <video width="100%" height="auto" preload="auto" autoplay="autoplay"
+                loop="loop" poster="pic/1447688410_interstellar.jpg">
+                <source src="pic/videoplayback.mp4" type="video/mp4"></source>
+                <source src="pic/videoplayback.webm" type="video/webm"></source>
+            </video>
+        </div>
+    </body>
+</html>
+
+
 <?php
-echo 100;
+
+
+
+
+
+
+
+
+
 ?>
